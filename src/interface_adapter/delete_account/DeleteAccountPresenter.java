090849b3748a4cfdc62670f6e12e48c4c8a451c3
milabhaloo <mila.bhaloo@mail.utoronto.ca>
2023-11-21 09:59:21 -0500

    added delete account output data class and made according changes

--- src/interface_adapter/delete_account/DeleteAccountPresenter.java
+++ src/interface_adapter/delete_account/DeleteAccountPresenter.java
@@ -3,19 +3,27 @@ package interface_adapter.delete_account;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.signup_login.SignUpLogInViewModel;
 import use_case.delete_account.DeleteAccountOutputBoundary;
+import use_case.delete_account.DeleteAccountOutputData;
+
 //TODO make sure the view models being used are correct (should i create a DeletedAccountViewModel since
 // there is no information to present)
 public class DeleteAccountPresenter implements DeleteAccountOutputBoundary {
     private final SignUpLogInViewModel signUpLogInViewModel;
+    private final DeleteAccountViewModel deleteAccountViewModel;
     private ViewManagerModel viewManagerModel;
     public DeleteAccountPresenter(SignUpLogInViewModel signUpLogInViewModel,
+                                  DeleteAccountViewModel deleteAccountViewModel,
                                   ViewManagerModel viewManagerModel){
         this.signUpLogInViewModel = signUpLogInViewModel;
+        this.deleteAccountViewModel = deleteAccountViewModel;
         this.viewManagerModel = viewManagerModel;
     }
     @Override
-    public void prepareDeletedAccountView(){
-        this.viewManagerModel.setActiveView(signUpLogInViewModel.getViewName());
-        this.viewManagerModel.firePropertyChanged();
+    public void prepareDeletedAccountView(DeleteAccountOutputData deletedUser){
+        String username = deletedUser.getDeletedUser();
+        DeleteAccountState state = deleteAccountViewModel.getState();
+        state.setUser(username);
+        deleteAccountViewModel.setState(state);
+        deleteAccountViewModel.firePropertyChanged();
     }
 }
--- src/interface_adapter/delete_account/DeleteAccountViewModel.java
+++ src/interface_adapter/delete_account/DeleteAccountViewModel.java
@@ -1,4 +1,25 @@
 package interface_adapter.delete_account;
 
-public class DeleteAccountViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class DeleteAccountViewModel extends ViewModel {
+    private DeleteAccountState state = new DeleteAccountState();
+    public DeleteAccountViewModel() { super("delete account"); }
+    public void setState(DeleteAccountState state) {
+        this.state = state;
+    }
+    public DeleteAccountState getState() { return this.state; }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state );
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
