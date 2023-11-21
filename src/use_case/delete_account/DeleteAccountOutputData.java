00f9f47c01c23601b94140047aba29aa7e899811
milabhaloo <mila.bhaloo@mail.utoronto.ca>
2023-11-21 10:22:33 -0500

    implemeted code for output data so it stores a string username

--- src/use_case/delete_account/DeleteAccountOutputData.java
+++ src/use_case/delete_account/DeleteAccountOutputData.java
@@ -1,2 +1,11 @@
-package use_case.delete_account;public class DeleteAccountOutputData {
+package use_case.delete_account;
+
+public class DeleteAccountOutputData {
+    private String deletedUser;
+    public DeleteAccountOutputData(String username){
+        this.deletedUser = username;
+    }
+    public String getDeletedUser(){
+        return this.deletedUser;
+    }
 }
