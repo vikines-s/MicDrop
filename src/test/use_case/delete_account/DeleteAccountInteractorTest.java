e63d6352da9531e8c4826ab0ccafcb23eb0140ee
milabhaloo <mila.bhaloo@mail.utoronto.ca>
2023-12-03 19:50:56 -0500

    edited the delete account interactor test to use common user

--- src/test/use_case/delete_account/DeleteAccountInteractorTest.java
+++ src/test/use_case/delete_account/DeleteAccountInteractorTest.java
@@ -2,13 +2,17 @@ package use_case.delete_account;
 
 import data_access.FileUserSpotifyAccessObject;
 import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeleteAccountInteractorTest {
     void successTest() {
         DeleteAccountDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
-        ((InMemoryUserDataAccessObject) userRepository).save("Mila");
+        CommonUserFactory userFactory = new CommonUserFactory();
+        User user = userFactory.create("Mila");
+        userRepository.save(user);
         DeleteAccountOutputBoundary successPresenter = new DeleteAccountOutputBoundary() {
             @Override
             public void prepareDeletedAccountView(DeleteAccountOutputData user) {
--- src/use_case/delete_account/DeleteAccountDataAccessInterface.java
+++ src/use_case/delete_account/DeleteAccountDataAccessInterface.java
@@ -3,6 +3,7 @@ package use_case.delete_account;
 import entity.User;
 public interface DeleteAccountDataAccessInterface {
     User get(String username);
+    void save(User user);
     void deleteAccount(String username);
     boolean existsByName(String username);
 }
