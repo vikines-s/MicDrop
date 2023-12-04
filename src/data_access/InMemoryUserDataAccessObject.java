1bc62470230a22042f598f8b42fbf69687118ca7
milabhaloo <mila.bhaloo@mail.utoronto.ca>
2023-12-03 19:50:15 -0500

    implemented the login interactor test

--- src/data_access/InMemoryUserDataAccessObject.java
+++ src/data_access/InMemoryUserDataAccessObject.java
@@ -2,24 +2,36 @@ package data_access;
 
 import entity.User;
 import use_case.delete_account.DeleteAccountDataAccessInterface;
+import use_case.login.LogInSpotifyAccessInterface;
+import use_case.login.LogInUserDataAccessInterface;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class InMemoryUserDataAccessObject implements DeleteAccountDataAccessInterface {
-    public Map<String, String> accounts = new HashMap<>();
-    public void save(String username) {
-        accounts.put(username, username);
-    }
+public class InMemoryUserDataAccessObject implements DeleteAccountDataAccessInterface,
+        LogInUserDataAccessInterface, LogInSpotifyAccessInterface {
+    public Map<String, User> accounts = new HashMap<>();
     public boolean existsByName(String identifier) {
         return accounts.containsKey(identifier);
     }
-
-    public User get(String username) {
-        return null;
+    @Override
+    public void save(User user) {
+        accounts.put(user.getName(), user);
     }
-
+    public User get(String username) {
+        return accounts.get(username);
+    }
     public void deleteAccount(String username) {
         accounts.remove(username);
+    }
+
+    @Override
+    public void updateUserData(User user) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hi");
+        user.setTopGenres(list);
+        user.setTopTracks(list);
+        user.setTopArtists(list);
     }
 }
