0c5dad71308af52117b0924ab773f9b82a6c9589
milabhaloo <mila.bhaloo@mail.utoronto.ca>
2023-12-06 15:49:47 -0500

    completed implementation of matches interactor test

--- src/test/use_case/matches/MatchesInteractorTest.java
+++ src/test/use_case/matches/MatchesInteractorTest.java
@@ -8,8 +8,7 @@ import use_case.matches.*;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MatchesInteractorTest {
     void successTest() {
@@ -28,12 +27,16 @@ public class MatchesInteractorTest {
         MatchesOutputBoundary successPresenter = new MatchesOutputBoundary() {
             @Override
             public void prepareSuccessView(MatchesOutputData matchesOutputData) {
-                assertEquals(matchesOutputData.)
+                assertEquals(matchesOutputData.getMatch1(), "Match 1:\n# of matching tracks : 5\nUser's email: kam@mail.utoronto.ca");
+                assertEquals(matchesOutputData.getMatch2(), "");
+                assertEquals(matchesOutputData.getMatch3(), "");
+                assertEquals(matchesOutputData.getMatch4(), "");
+                assertEquals(matchesOutputData.getMatch5(), "");
+                assertEquals(matchesOutputData.getMatchType(), "tracks");
             }
-
             @Override
             public void prepareFailView(String error) {
-
+                fail("Use case failure is unexpected");
             }
         };
         MatchesInputData inputData = new MatchesInputData("Mila", new TracksAlgorithm());
